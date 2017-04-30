5da08bff8218706a3b5c067463e6d3291071ed4c
sshado <sshadout@gmail.com>
2017-04-30 18:42:13 -0400

    All loops working.

--- builder.js
+++ builder.js
@@ -21,13 +21,29 @@ var unitArray = []; // Store units for return.
 		Game.creeps.pix.memory.role = 'builder';
 		unitArray.push(Game.creeps.pix);
 	}
-	// Check to see if pix is alive or not, then create him. 
+	// Check to see if pix0 is alive or not, then create him. 
 	if(spawn.canCreateCreep([WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE],'pix0') == 0){
 		spawn.createCreep([WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE],'pix0');
 		
 		// Push the creep object onto the array.
 		Game.creeps.pix0.memory.role = 'builder';
 		unitArray.push(Game.creeps.pix0);
+	}
+	// Check to see if pix00 is alive or not, then create him. 
+	if(spawn.canCreateCreep([WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE],'pix00') == 0){
+		spawn.createCreep([WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE],'pix00');
+		
+		// Push the creep object onto the array.
+		Game.creeps.pix00.memory.role = 'builder';
+		unitArray.push(Game.creeps.pix00);
+	}
+	// Check to see if pix000 is alive or not, then create him. 
+	if(spawn.canCreateCreep([WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE],'pix000') == 0){
+		spawn.createCreep([WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE],'pix000');
+		
+		// Push the creep object onto the array.
+		Game.creeps.pix000.memory.role = 'builder';
+		unitArray.push(Game.creeps.pix000);
 	}
 }
  
@@ -41,6 +57,16 @@ var unitArray = []; // Store units for return.
 	if(Game.creeps.pix0){
 		Game.creeps.pix0.memory.role = 'builder';
 		unitArray.push(Game.creeps.pix0);
+	}
+	// Push the creep object onto the array.
+	if(Game.creeps.pix00){
+		Game.creeps.pix00.memory.role = 'builder';
+		unitArray.push(Game.creeps.pix00);
+	}
+	// Push the creep object onto the array.
+	if(Game.creeps.pix000){
+		Game.creeps.pix000.memory.role = 'builder';
+		unitArray.push(Game.creeps.pix000);
 	}
 }
 
